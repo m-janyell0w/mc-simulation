fe5d9a68a780e7f7108a33f314a8792793574dd0
Micha Ianniello <micha.ianniello@gmail.com>
2024-05-20 15:35:37 +0200

    fix callbacks to display simulation results

--- mc_simulation/callbacks.py
+++ mc_simulation/callbacks.py
@@ -1,29 +1,29 @@
 from dash.dependencies import Input, Output, State
-from dash import dcc, callback
+from dash import dcc, callback, no_update
 import dash_html_components as html
 import numpy as np
 import plotly.graph_objs as go
 
-from .app import app
+# from .app import app
 from .simulation import MonteCarloSimulation
 
 
-@app.callback(
-    Output("simulation-graph-container", "children"),
+@callback(
+    Output("simulation-graph-container", "figure"),
     Input("submit-simu", "n_clicks"),
     State("num-simulations", "value"),
-    State("num-days", "value"),
+    State("num-months", "value"),
     State("initial-investment", "value"),
-    State("dropdown-stock-ticker", "value"),
+    Input("dropdown-stock-ticker", "value"),
     State("monthly-investment", "value"),
-    prevent_initial_call=True
 )
-def start_simulation(n_clicks, num_simulations, num_days, initial_investment, stock_ticker, monthly_investment):
-    print("Starting simulation...")
+def run_simulation(n_clicks, num_simulations, num_months, initial_investment, stock_ticker, monthly_investment):
+    
+    fig = init_empty_fig(title=f"Monte Carlo Simulation for Asset: {stock_ticker}")
     if n_clicks == 0:
-        return html.Div("Simulation not started yet.")
+        return fig
 
-    num_days = num_days * 22  # Convert number of months to trading days
+    num_days = num_months * 22  # Convert number of months to trading days
 
     mcs = MonteCarloSimulation(
         symbol=stock_ticker,
@@ -40,32 +40,43 @@ def start_simulation(n_clicks, num_simulations, num_days, initial_investment, st
     if results is None:
         return html.Div("No data available for simulation.")
 
-    fig = plot_simulation_results(results)
+    fig = plot_simulation_results(fig, results, num_days, num_simulations,
+                                  stock_ticker)
+    return fig
 
-    fig.update_layout(
-        title='Monte Carlo Simulation of Stock Portfolio',
-        xaxis_title='Days',
-        yaxis_title='Portfolio Value',
-        template='plotly_dark'
-    )
+@callback(
+    Output("simulation-progess-container", "children"),
+    Input("submit-simu", "n_clicks"),
+    Input("simulation-graph-container", "figure"),
+    State("num-simulations", "value"),
+    State("dropdown-stock-ticker", "value"),
+    prevent_initial_call=True
+)
+def show_simulation_progress(n_clicks, graph_container, num_simulations, stock_ticker):
+    if n_clicks == 0:
+        return no_update
+    if isinstance(graph_container, go.Figure):
+        return html.Div(f"Simulation completed for {stock_ticker}.")
 
-    return dcc.Graph(figure=fig)
+    return html.Div(f"Simlutation started. Running {num_simulations} simulations for {stock_ticker} ...")
 
-def plot_simulation_results(results: np.ndarray, num_days: int, num_simulations: int):
+def plot_simulation_results(fig: go.Figure, results: np.ndarray, num_days: int,
+                            num_simulations: int, ticker_symbol: str, max_simulations=50):
     """
     Plot the results of the Monte Carlo simulation. This function plots the max and min simulations along with a subset
     of other simulations for comparison.
     """
+    print("Create plot of simulation results.")
     final_values = results[:, -1]
     max_index = final_values.argmax()
     min_index = final_values.argmin()
+    median_index = np.argsort(final_values)[len(final_values) // 2]    
 
-    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=list(range(num_days + 1)), y=results[max_index], mode='lines', name='Max'))
+    fig.add_trace(go.Scatter(x=list(range(num_days + 1)), y=results[min_index], mode='lines', name='Min'))
+    fig.add_trace(go.Scatter(x=list(range(num_days + 1)), y=results[median_index], mode='lines', name='Median'))
 
-    fig.add_trace(go.Scatter(x=list(range(num_days + 1)), y=results[max_index], mode='lines', name='Max Simulation'))
-    fig.add_trace(go.Scatter(x=list(range(num_days + 1)), y=results[min_index], mode='lines', name='Min Simulation'))
-
-    num_to_plot = min(num_simulations, 50)
+    num_to_plot = min(num_simulations, max_simulations)
     indices = set([max_index, min_index])  # Ensure max and min are included
     while len(indices) < num_to_plot:
         indices.add(np.random.randint(num_simulations))
@@ -73,8 +84,27 @@ def plot_simulation_results(results: np.ndarray, num_days: int, num_simulations:
     for i in indices:
         if i != max_index and i != min_index:  # Avoid re-plotting max and min
             fig.add_trace(go.Scatter(x=list(range(num_days + 1)), y=results[i], mode='lines', name=f'Simulation {i+1}'))
+    
+    fig.update_xaxes(tickvals=np.arange(0, num_days, 252), ticktext=np.arange(0, num_days//252))
+    fig.update_yaxes(range=[0, 1.1 * results.max()])
     return fig
 
+
+def init_empty_fig(xaxis_title="Years", yaxis_title="Portfolio Value",
+                    title="Monte Carlo Simulation Results"):
+    fig = go.Figure()
+    fig.update_layout(
+        title=title,
+        xaxis_title=xaxis_title,
+        yaxis_title=yaxis_title,
+    )
+    return fig
+
+mc_simulation_callbacks = [
+    run_simulation,
+    show_simulation_progress,
+    plot_simulation_results,
+]
 
 @callback(
     Output("test-output", "children"),
@@ -83,4 +113,4 @@ def plot_simulation_results(results: np.ndarray, num_days: int, num_simulations:
 )
 def test_button(n_clicks):
     print(f"Button clicked {n_clicks} times.")
-    return f"Button clicked {n_clicks} times."
+    return f"# Button clicked {n_clicks} times."
