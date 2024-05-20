49e5fbb936d8c7b2698c51839abf5fe002f68913
Micha Ianniello <micha.ianniello@gmail.com>
2024-05-20 15:34:31 +0200

    fix import of callbacks

--- mc_simulation/app.py
+++ mc_simulation/app.py
@@ -1,8 +1,9 @@
 import dash
 from mc_simulation.layout import APP_LAYOUT
 import dash_bootstrap_components as dbc
+from mc_simulation.callbacks import mc_simulation_callbacks
 
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.COSMO])
 app.layout = APP_LAYOUT
 app.run_server(debug=True)
